6e28e1b63f45a7ff3b4e03b237663edd06390747
peters <peter.stritzinger@abf.at>
2022-04-05 16:17:34 +0200

    initial add

---
+++ src/main/java/com/example/releaseplugin/ReleasepluginApplication.java
@@ -0,0 +1,13 @@
+package com.example.releaseplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasepluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasepluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/releaseplugin/ReleasepluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releaseplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasepluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
